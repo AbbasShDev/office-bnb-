36d207d7f94036675cab9d6cd5946fcccfa59249
abbasShDev <abbas20alzaeem@gmail.com>
2021-12-06 13:14:37 +0300

    Done listing offices and show single office

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -4,17 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\OfficeResource;
 use App\Models\Office;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class OfficeController extends Controller {
 
-    public function index(): AnonymousResourceCollection
+    public function index(Request $request): AnonymousResourceCollection
     {
         $offices = Office::query()
-            ->latest('id')
-            ->get();
+            ->where('approval_status', Office::APPROVAL_APPROVED)
+            ->where('hidden', false)
+            ->when($request->host_id, fn($builder) => $builder->where('user_id', $request->host_id))
+            ->when($request->user_id,
+                fn($builder) => $builder->whereRelation('reservations', 'user_id', '=', $request->user_id)
+            )
+            ->when(
+                $request->lat && $request->lng,
+                fn($builder) => $builder->nearestTo($request->lat, $request->lng),
+                fn($builder) => $builder->orderBy('id', 'ASC'),
+            )
+            ->with(['images', 'tags', 'user'])
+            ->withCount(['reservations' => fn($builder) => $builder->where('status', Reservation::STATUS_ACTIVE)])
+            ->paginate(20);
 
         return OfficeResource::collection($offices);
+    }
+
+    public function show(Office $office)
+    {
+        $office->loadCount(['reservations' => fn($builder) => $builder->where('status', Reservation::STATUS_ACTIVE)])
+            ->load(['images', 'tags', 'user']);
+
+        return OfficeResource::make($office);
     }
 }
